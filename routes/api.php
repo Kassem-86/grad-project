b21dd53bd7e83a3eb256f837bd01ad05c0bbf24a
Kassem-86 <ziadkassem54@gmail.com>
2026-06-17 16:43:02 +0300

    auth pass

--- routes/api.php
+++ routes/api.php
@@ -71,8 +71,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/profile/{id}', [UserController::class, 'showProfile']);
     Route::post('/chatbot/ask', [App\Http\Controllers\ChatbotController::class, 'askChatbot']);// Route عشان تسأل الشات بوت (ممكن تحطه في مكان تاني لو حابب)
     Route::get('/chatbot/history', [ChatbotController::class, 'getChatHistory']);
-Route::post('/password/verify', [AuthController::class, 'verifyCurrentPassword']);
-Route::post('/password/update', [AuthController::class, 'updateNewPassword']);
+// Route::post('/password/verify', [AuthController::class, 'verifyCurrentPassword']);
+// Route::post('/password/update', [AuthController::class, 'updateNewPassword']);
     // Preferred standardized endpoint for retrieving the authenticated user
     Route::get('/me', [AuthController::class, 'me']);
     Route::delete('/delete-me', [AuthController::class, 'deleteUser']);
